4cd0040712d32496889244223a4ab9edeb7f9019
DenEdwards <denzelsedwards@gmail.com>
2021-08-26 16:36:38 -0400

    Connection b/w Teachable and Microbit established

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,8 +1,32 @@
-import { io } from 'socket.io-client';
-import socketClient from 'socket.io-client';
+// const socket = io();
+// const socket = io('http://localhost:8080');
 
-// const socket = socketClient();
-const socket = socketClient('http://127.0.0.1:8080');
-socket.on('connect', () => {
-	console.log('hello');
-});
+// socket.on('connect', () => {});
+// let URL = 'https://teachablemachine.withgoogle.com/models/TmHiXRkQy/';
+
+submitUrl.onclick = (event) => {
+	if (validURL(teachableUrl.value)) {
+		found = false;
+		openPort();
+		init(teachableUrl.value);
+	} else {
+		alert('invalid URL');
+	}
+};
+
+function serialSubmit(classPrediction) {
+	writeToSerial(classPrediction);
+}
+
+function validURL(str) {
+	var pattern = new RegExp(
+		'^(https?:\\/\\/)?' + // protocol
+			'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+			'((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+			'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+			'(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+			'(\\#[-a-z\\d_]*)?$',
+		'i'
+	); // fragment locator
+	return !!pattern.test(str);
+}
--- server.js
+++ server.js
@@ -1,28 +1,25 @@
 const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+	cors: {
+		origin: '*',
+		// methods: ['GET', 'POST'],
+	},
+});
 const path = require('path');
-const http = require('http');
-const socketIo = require('socket.io');
 if (process.env.NODE_ENV !== 'production') {
 	require('dotenv').config();
 }
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server, {
-	cors: {
-		origin: '*',
-		methods: ['GET', 'POST'],
-	},
-});
-
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, 'public/pages/index.html'));
-});
-
-io.on('connection', (socket) => {
-	console.log(socket.id);
+	res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 server.listen(process.env.PORT || 8080, () => console.log('Server started'));
+
+io.on('connect', (socket) => {
+	console.log('User connected: ' + socket.id);
+});
