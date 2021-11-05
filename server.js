9eee24745ffae0100b875b7a598b8607ffe0571c
DenEdwards <denzelsedwards@gmail.com>
2021-11-05 11:51:16 -0400

    Resetting origin for webhooks

--- server.js
+++ server.js
@@ -26,9 +26,9 @@ app.post('/git', (req, res) => {
 	if (req.headers['x-github-event'] === 'push' && crypto.timingSafeEqual(Buffer.from(sig), Buffer.from(req.headers['x-hub-signature']))) {
 		res.sendStatus(200);
 		const commands = [
-			'git fetch origin master',
-			'git reset --hard origin/master',
-			'git pull origin master --force',
+			'git fetch origin main',
+			'git reset --hard origin/main',
+			'git pull origin main --force',
 			'npm install',
 			// your build commands here
 			'refresh',
